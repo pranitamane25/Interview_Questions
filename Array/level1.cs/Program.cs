095561f10071efa72924fbe3cb7aa7ea2793472a
pranitamane25 <pranitamane2506@gmail.com>
2026-01-14 10:14:54 +0530

    print array elements

---
+++ Array/level1.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class program
+{
+    public static void Main()
+    {
+        int [] arr={5,10,15,20,25,30};
+        for (int i = 0; i <= arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
