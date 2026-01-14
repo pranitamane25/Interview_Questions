e0dbdae42015b081ed9050ca96d6bece91a121be
pranitamane25 <pranitamane2506@gmail.com>
2026-01-14 20:42:36 +0530

    sum of the array elements

---
+++ Array/level2.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿//sum of the array
+
+using System ;
+
+ public class program
+{
+    public static void Main()
+    {
+        int [] arr={2,4,6,8,10};
+       int sum=0;
+       for(int i = 0; i < arr.Length; i++)
+        {
+            sum=sum+arr[i];
+        }
+        Console.WriteLine(sum);
+    }
+
+}
