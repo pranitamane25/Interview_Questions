537a274bfc07065d5c506ed6cb47371487a4625a
pranitamane25 <pranitamane2506@gmail.com>
2026-01-14 22:36:33 +0530

    largest number of an array

---
+++ Array/level3.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class program
+{
+    public static void Main()
+    {
+        int []arr={10,12,4,20,78};
+        int max=arr[0];
+        for(int i = 1; i < arr.Length; i++)
+        {
+        if  (arr[i] >max)
+        {
+        max=arr[i];
+    }
+ }
+    Console.WriteLine("Largest element"+max);
+    }
+}
